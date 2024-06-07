d1ba3dfeeb9dd8b4d5ad0d5f946680d3850e612f
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-06-07 16:20:09 +0200

    ADDED but not finished playRound function with game logic (currently broken and work in progres)

--- README.md
+++ README.md
@@ -6,20 +6,29 @@ This README.md file will be used to explain my experience and the process and wi
 
 
 PSEUDOCODE:
--
--
--
+
+
+
 getComputerChoice
 -
 CREATE local let choice variable
 GENERATE random number between 0 and 1
 ASSIGN 0-0.33, 0.34-0.67, 0.68-1 to Rock, Paper and Scissors via if-else statement
 RETURN choice
--
--
--
+
+
+
 getHumanChoice
 -
 CREATE local let choice variable
 PROMPT the user for his choice
 RETURN choice
+
+
+playRound
+-
+RECEIVE getHumanChoice and getComputerChoice as arguments
+TRASNFORM getHumanChoice to lower case
+TRANSFORM the first letter of getHumanChoice to upper case
+COMPARE the choices 
+DECLARE a winner via if-else statement
--- scripts.js
+++ scripts.js
@@ -15,15 +15,61 @@ function getComputerChoice() {
     }
 }
 
-console.log(`The computer chose: ${getComputerChoice()}`);
-
 function getHumanChoice() {
     let choice = prompt("Tell me your choice.");
     return "Your choice was: " + choice;
 }
 
-console.log(getHumanChoice());
-
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (typeof humanChoice === 'string') {
+        humanChoice = humanChoice.toLowerCase();
+        humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+    } else {
+        console.log('Error: humanChoice is not a string');
+    }
+    // Player choice Rock scenario
+    if (humanChoice === "Rock") {
+        if (computerChoice === "Rock") {
+            return "Both players chose the same. This is a tie.";
+        } else if (computerChoice === "Scissors") {
+            humanScore++;
+            return "Congratulation your Rock smashed the computer's Scissors!"
+        } else if (computerChoice === "Paper") {
+            computerScore++;
+            return "That's a shame... The computer's Paper wrapped around your Rock!"
+        } else return "Something went wrong. Try again."
+
+    // Player choice Paper scenario
+    } else if (humanChoice === "Paper") {
+        if (computerChoice === "Paper") {
+            return "Both players chose the same. This is a tie.";
+        } else if (computerChoice === "Rock") {
+            humanScore++;
+            return "Congratulation your Paper wrapped around the computer's Rock!"
+        } else if (computerChoice === "Scissors") {
+            computerScore++;
+            return "That's a shame... The computer's Scissors cut your Paper in pieces!"
+        }
+    // Player choice Scissors scenario
+    } else if (humanChoice === "Scissors") {
+        if (computerChoice === "Scissors") {
+            return "Both players chose the same. This is a tie.";
+        } else if (computerChoice === "Paper") {
+            humanScore++;
+            return "Congratulation your Scissors cut the computer's Paper to pieces!"
+        } else if (computerChoice === "Rock") {
+            computerScore++;
+            return "That's a shame... The computer's Rock smashed your Scissors!"
+        } else return "Something went wrong. Try again."
+
+    } else return "Something went wrong, try again.";
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound());
